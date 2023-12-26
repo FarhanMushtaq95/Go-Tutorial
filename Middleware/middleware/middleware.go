83755bd33a02d80dfa9b0e6e91805d6d7adf695b
farhanmushtaq95 <farhanmushtaq859@gmail.com>
2023-12-26 19:45:14 +0500

    Added Middleware for Auth

---
+++ Middleware/middleware/middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func Authenticate(c *gin.Context) {
+	if !(c.Request.Header.Get("token") == "auth") {
+		c.AbortWithStatusJSON(500, gin.H{
+			"Message": "Token not found",
+		})
+		return
+	}
+	c.Next()
+}
